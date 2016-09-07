2deec2abbb7f1aab53b8b341b346e2c53e385e9e
Bruce Tsao <prgbruce@gmail.com>
2016-09-07 20:07:37 +0800

    commit all

---
+++ 201609/toneMusic/toneMusic.ino
@@ -0,0 +1,84 @@
+#include "pitches.h"
+
+int melodyPin=11;//設置控制蜂鳴器的數位IO腳
+int melody[] = {
+  NOTE_E7, NOTE_E7, 0, NOTE_E7, 
+  0, NOTE_C7, NOTE_E7, 0,  
+  NOTE_G7, 0, 0,  0,
+  NOTE_G6, 0, 0, 0, 
+
+  NOTE_C7, 0, 0, NOTE_G6, 
+  0, 0, NOTE_E6, 0, 
+  0, NOTE_A6, 0, NOTE_B6, 
+  0, NOTE_AS6, NOTE_A6, 0, 
+
+  NOTE_G6, NOTE_E7, NOTE_G7, 
+  NOTE_A7, 0, NOTE_F7, NOTE_G7, 
+  0, NOTE_E7, 0,NOTE_C7, 
+  NOTE_D7, NOTE_B6, 0, 0,
+
+  NOTE_C7, 0, 0, NOTE_G6, 
+  0, 0, NOTE_E6, 0, 
+  0, NOTE_A6, 0, NOTE_B6, 
+  0, NOTE_AS6, NOTE_A6, 0, 
+
+  NOTE_G6, NOTE_E7, NOTE_G7, 
+  NOTE_A7, 0, NOTE_F7, NOTE_G7, 
+  0, NOTE_E7, 0,NOTE_C7, 
+  NOTE_D7, NOTE_B6, 0, 0
+};
+
+//Mario main them tempo
+int tempo[] = {
+  12, 12, 12, 12, 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12, 
+
+  12, 12, 12, 12,
+  12, 12, 12, 12, 
+  12, 12, 12, 12, 
+  12, 12, 12, 12, 
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+};
+
+
+ int mariolen = sizeof(melody) / sizeof(int) ;
+ 
+ 
+void setup() 
+{ 
+pinMode(melodyPin,OUTPUT);//設置數位IO腳模式，OUTPUT為轀出 
+} 
+void loop() 
+{ 
+   playMario() ; 
+} 
+
+void playMario()
+{
+   int noteDuration ;
+  for(int mariopos=0; mariopos <mariolen; mariopos++)
+    {
+         noteDuration = 1000/tempo[mariopos]; 
+        tone(melodyPin, melody[mariopos],noteDuration); 
+        delay(noteDuration*1.3);
+    }
+
+
+  
+}
